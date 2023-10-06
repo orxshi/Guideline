764a6240fa3336e7a97614c04e86e9c9c2f6c9d5
orxshi <orxan.shibli@gmail.com>
2023-10-06 21:55:05 +0300

    >

--- toc.js
+++ toc.js
@@ -1,8 +1,8 @@
-$('h1').each(function () {
+$('h2').each(function () {
     this.id = $(this).text();
 });
 
-$('h2').each(function () {
+$('h3').each(function () {
     this.id = $(this).text();
 });
 
@@ -41,20 +41,46 @@ $('h2').each(function () {
 
 
 
+//var toc = document.getElementById('toc');
+//var h1h2 = document.querySelectorAll("h1, h2");
+//for (var i = 0; i < h1h2.length; i++) {
+//    if (h1h2[i].nodeName === "H2") {        
+//        var li = document.createElement("li");
+//        var createA = document.createElement('a');
+//        var createAText = document.createTextNode(h1h2[i].textContent);        
+//        createA.setAttribute('href', '#' + h1h2[i].id);
+//        createA.appendChild(createAText);
+//        li.appendChild(createA);
+//        //  li.appendChild(document.createTextNode(h1h2[i].textContent));
+//        toc.appendChild(li);
+//    }
+//    else {
+//        var textNode = document.createTextNode(h1h2[i].textContent);
+//        toc.appendChild(textNode);
+//    }
+//
+//}
+
 var toc = document.getElementById('toc');
-var h1h2 = document.querySelectorAll("h1, h2");
+var h1h2 = document.querySelectorAll("h2, h3");
 for (var i = 0; i < h1h2.length; i++) {
-    if (h1h2[i].nodeName === "H2") {        
-        var li = document.createElement("li");
-        var createA = document.createElement('a');
-        var createAText = document.createTextNode(h1h2[i].textContent);        
-        createA.setAttribute('href', '#' + h1h2[i].id);
-        createA.appendChild(createAText);
-        li.appendChild(createA);
-        //  li.appendChild(document.createTextNode(h1h2[i].textContent));
-        toc.appendChild(li);
+    if (h1h2[i].nodeName === "H3") {        
+        if (h1h2[i].id == "Contents") {continue;}
+        if (h1h2[i].id == "Important websites") {continue;}
+            //console.log(h1h2[i].id);
+            var li = document.createElement("li");
+            var createA = document.createElement('a');
+            var createAText = document.createTextNode(h1h2[i].textContent);        
+            createA.setAttribute('href', '#' + h1h2[i].id);
+            createA.appendChild(createAText);
+            li.appendChild(createA);
+            //  li.appendChild(document.createTextNode(h1h2[i].textContent));
+            toc.appendChild(li);
     }
     else {
+        if (h1h2[i].id == "Contents") {continue;}
+        if (h1h2[i].id == "Important websites") {continue;}
+            console.log(h1h2[i].id);
         var textNode = document.createTextNode(h1h2[i].textContent);
         toc.appendChild(textNode);
     }
